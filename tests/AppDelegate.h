6ed2a758b87c2d69ea6ed107c9a78f05ed88fa95
Sanchit Bahal <sanchit.bahal@live.com>
2012-08-21 10:31:02 +0530

    Added support for unit tests.

---
+++ tests/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tests
+//
+//  Created by Sanchit on 21/08/12.
+//  Copyright (c) 2012 ThoughtWorks Technologies (India) Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tests/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tests
+//
+//  Created by Sanchit on 21/08/12.
+//  Copyright (c) 2012 ThoughtWorks Technologies (India) Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    }
+}
